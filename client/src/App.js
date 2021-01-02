fcbf5ac97c6e3ff9e7d8c4a7b4c793be06ded8c2
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2021-01-02 17:51:55 +0100

    dockerize app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {TestAPI} from "./components/TestAPI";
+import {TestDatabase} from "./components/TestDatabase";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TestAPI></TestAPI>
+        <TestDatabase></TestDatabase>
 
       </header>
     </div>
--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3005
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const port = process.env.SERVER_PORT
 
 app.get('/test', (req, res) => {
     res.json({ message: 'This message comes from the server-container. (API Request)' })
 })
 
+app.get('/database', (req, res) => {
+
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Server's running at http://localhost:${port}`)
 })
