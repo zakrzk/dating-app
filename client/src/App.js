bfe15c6089b0489c624986debcd33a10a9374bfc
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2021-03-26 18:10:11 +0100

    install Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import {TestAPI} from "./components/TestAPI";
+import 'fontsource-roboto';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+
+import {Header} from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TestAPI></TestAPI>
+        {/*<TestAPI></TestAPI>*/}
+          <Header/>
+
       </header>
     </div>
   );
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import Button from '@material-ui/core/Button';
+
+import React, {Component} from 'react';
+
+export class Header extends Component {
+    render() {
+        return (
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+        );
+    }
+}
