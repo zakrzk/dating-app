e31586a86bcfe1fafc6937c8637239b0446c45ec
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2021-03-27 19:26:40 +0100

    deploy to ec2

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -10,36 +10,47 @@ function Card() {
         {
             id: 1,
             name: "John",
+            age: 20,
             level: 90
         },
         {
             id: 2,
+            age: 20,
             name: "Monica",
             level: 55
         },
         {
             id: 3,
-            name: "Gambrysia",
+            age: 20,
+            name: "Gambrysiaaaa",
             level: 100
         },
     ]);
 
 
     return (
-        <div style={{marginTop: "80px",}}>
-            {users.map(user => (
-                <TinderCard
-                    preventSwipe={['down', 'up']}
-                    className="card swipe"
-                    key={user.id}>
 
-                    <div className="card">
-                        <h2 className="card__name">{user.name}</h2>
-                        <h2 className="card__level">{user.level}</h2>
-                    </div>
+        <div style={{marginTop: "7vh"}}>
 
-                </TinderCard>
-            ))}
+            <div className="card__wrapper">
+                {users.map(user => (
+                    <TinderCard
+                        preventSwipe={['down', 'up']}
+                        className="card swipe"
+                        key={user.id}
+                    >
+                        <div className="wrapper">
+                            <div className="card__level">
+                                <h2>{user.level}</h2>
+                            </div>
+                            <div className="card__name">
+                                <h2>{user.name}</h2>
+                            </div>
+                        </div>
+
+                    </TinderCard>
+                ))}
+            </div>
         </div>
 
     );
