ccd0344bce0072203093e0a99874fd74d6e70027
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2021-03-30 01:08:58 +0200

    change chip color"

--- client/src/components/registration/Hobby.js
+++ client/src/components/registration/Hobby.js
@@ -10,9 +10,11 @@ function Hobby(props) {
         if (attribute === 'true') {
             inputEl.current.setAttribute('isclicked', "false");
             inputEl.current.style.backgroundColor = '#e0e0e0'
+            inputEl.current.style.color = 'rgba(0, 0, 0, 0.87)'
         } else {
             inputEl.current.setAttribute('isclicked', "true");
-            inputEl.current.style.backgroundColor = '#91c879'
+            inputEl.current.style.backgroundColor = '#87d37c'
+            inputEl.current.style.color = 'white'
         }
         console.log(event.target)
         console.log(inputEl.current)
