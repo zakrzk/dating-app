c5351a477c3f7a07f735cb24b7445afba582e293
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2021-04-09 16:54:14 +0200

    add dummy user data

---
+++ server/data.js
@@ -0,0 +1,67 @@
+export const users = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'john@gmail.com',
+        password: 'hashedPass',
+        age: 21,
+        gender: 'demiboy',
+        orientation: [
+            'female',
+            'male',
+            'bigender'
+        ],
+        profession: 'doctor',
+        hobbies: [
+            'soccer',
+            'handball',
+            'chess',
+            'math',
+            'music',
+            'forestry',
+            'design',
+            'dyi',
+            'mexican',
+            'shrimps',
+        ],
+        political: {
+            economics: 0,
+            diplomatic: -3,
+            civil: 3,
+            societal: 5
+        }
+    },
+    {
+        id: 2,
+        name: 'Monica',
+        email: 'monica@gmail.com',
+        password: 'hashedPass2',
+        age: 19,
+        gender: 'female',
+        orientation: [
+            'demiboy',
+            'male',
+            'female'
+        ],
+        profession: 'UI Designer',
+        hobbies: [
+            'soccer',
+            'handball',
+            'surfing',
+            'math',
+            'music',
+            'forestry',
+            'design',
+            'dyi',
+            'chemistry',
+            'sushi',
+            'shrimps',
+        ],
+        political: {
+            economics: -2,
+            diplomatic: 3,
+            civil: 4,
+            societal: 1
+        }
+    },
+]
