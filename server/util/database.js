d26fde81e295351168e2e8b1e30258c74f9cd125
Bartek Zakrzewski <bzakrzewski1@gmail.com>
2021-04-11 22:08:31 +0200

    wip: fix issues with .js

--- server/util/database.js
+++ server/util/database.js
@@ -6,5 +6,5 @@ export const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD, {
         dialect: 'mysql',
-        host: 'database'
+        host: process.env.DB_HOST
     });
